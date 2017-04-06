a15071d7b39ad70309344717b8f3ed4ee5bfb54f
suyu <kidinamoto@qq.com>
2017-04-06 13:42:25 +0800

    参考Github代码，在MySQL中创建新的表，存储Hive中读到的处理完的数据。

--- src/main/scala-2.11/sheshou/SparkToJDBC.scala
+++ src/main/scala-2.11/sheshou/SparkToJDBC.scala
@@ -1,14 +1,18 @@
 package sheshou
 
+import java.sql.{DriverManager, ResultSet}
 import java.util.Properties
 
 import org.apache.spark.{SparkConf, SparkContext}
+import scala.collection.mutable.MutableList
 
 /**
   * Created by b on 17/4/6.
   */
 
 object SparkToJDBC {
+
+  case class Person(key:Int,fname:String,lname:String,address:String)
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("JDBCrExample").setMaster("local[*]")
@@ -20,12 +24,33 @@ object SparkToJDBC {
     prop.setProperty("user", "root")
     prop.setProperty("password", "root")
 
-    val data = sc.parallelize(List(("iteblog", 30), ("iteblog", 29), ("com", 40), ("bt", 33), ("www", 23)))
+    //get data from hive
+    Class.forName("org.apache.hive.jdbc.HiveDriver");
+    val conn = DriverManager.getConnection("jdbc:hive2://192.168.1.23:10000/default", "admin", "123456")
+
+    //get input table
+    val res: ResultSet = conn.createStatement
+      .executeQuery("SELECT * FROM person_hbase")
+
+    //fetch all the data
+    val fetchedRes = MutableList[Person]()
+
+    while(res.next()) {
+      var rec = Person(
+        res.getInt("key"),
+        res.getString("fname"),
+        res.getString("lname"),
+        res.getString("address"))
+      fetchedRes += rec
+    }
 
     import sqlContext.implicits._
 
-    val df = data.toDF.toDF("id","val")
-    val dfWriter = df.write.mode("append").option("driver", "com.mysql.jdbc.Driver")
-    dfWriter.jdbc("jdbc:mysql://localhost:3306/test", "my_new_table", prop) //df.createJDBCTable(url, "sparktomysql", false)
+    //val df = data.toDF.toDF("id","val")
+    val df = fetchedRes.toDF
+
+    val dfWriter = df.write.mode("overwrite").option("driver", "com.mysql.jdbc.Driver")
+    dfWriter.jdbc("jdbc:mysql://localhost:3306/test", "people", prop)
+
   }
 }
