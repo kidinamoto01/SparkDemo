3b0b1e9c8d3d496cf59bf82b6a9221039af15b58
suyu <kidinamoto@qq.com>
2017-04-11 16:33:12 +0800

    参考Github代码，j解析并保存Json数据。

--- src/main/scala-2.11/WordCount.scala
+++ src/main/scala-2.11/WordCount.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -7,11 +8,19 @@ object WordCount {
 
   def main(args: Array[String]) {
     val logFile = "/usr/local/share/spark-2.1.0-bin-hadoop2.6/README.md" // Should be some file on your system
+    val filepath = "/Users/b/Documents/andlinks/sheshou/log/0401log3(1).txt"
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    val sqlContext = new SQLContext(sc)
+    //read json file
+    val file =sqlContext.read.json(filepath).toDF()
+    println(file.first())
+    file.coalesce(1).write
+      .format("com.databricks.spark.csv")
+      .mode("append")
+      .option("delimiter","\t")
+      .save("/Users/b/Documents/sheshou")
+
+
   }
 }
